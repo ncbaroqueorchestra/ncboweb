961e931199af12f8f103fe5414338b99a704a2f3
Steve Hall <srhusa@outlook.com>
2024-12-06 11:26:50 +0000

    changed sort logic

--- refresh_event_data.py
+++ refresh_event_data.py
@@ -1,6 +1,5 @@
 # Refresh event data from NCBO Airtable
 from airtable import Airtable
-import json
 import yaml
 from datetime import datetime
 import pytz
@@ -9,10 +8,10 @@ import os
 airtable_base_id = os.environ.get('AIRTABLE_BASE_ID')
 airtable_api_key = os.environ.get('AIRTABLE_API_KEY')
 
-print(f"AIRTABLE_BASE_ID: {airtable_base_id}"  )
-print(f"AIRTABLE_API_KEY: {airtable_api_key}"  )
+print(f"AIRTABLE_BASE_ID: {airtable_base_id}")
+print(f"AIRTABLE_API_KEY: {airtable_api_key}")
 if airtable_base_id is None or airtable_api_key is None:
-    print(f"Environment variables are not set.")
+    print("Environment variables are not set.")
     exit(1)
 
 airtable = Airtable(airtable_base_id, 'Events', api_key=airtable_api_key)
@@ -25,7 +24,7 @@ for record in records:
     new_rec = {}
     fields = record['fields']
 
-    #Rename fields
+    # Rename fields
     new_rec['heading'] = fields.get('EventName', '')
 
     # convert new_rec['DateTime'] to MM/DD/YYYY
@@ -43,6 +42,7 @@ for record in records:
     ny_dt = utc_dt.astimezone(ny_tz)
 
     new_rec['eventdate'] = ny_dt.strftime('%m/%d/%Y')
+    new_rec['sortdate'] = ny_dt.strftime('%Y%m%d')
     new_rec['eventtime'] = ny_dt.strftime('%I:%M %p')
     new_rec['venue'] = fields.get('Venue', '')
     new_rec['description'] = fields.get('Event info', '')
@@ -56,19 +56,20 @@ for record in records:
     if fields.get('EventDetailsURL', '') != '':
         new_rec['eventdetailsurl'] = fields.get('EventDetailsURL', '')
 
-
-    #only append if date has not passed
+    # only append if date has not passed
     if utc_dt > datetime.now(pytz.utc):
         formatted_records.append(new_rec)
     else:
         expired_events.append(new_rec)
 
 # sort formatted_records by eventdate
-formatted_records = sorted(formatted_records, key=lambda k: k['eventdate'])
-expired_events = sorted(expired_events, key=lambda k: k['eventdate'],
+formatted_records = sorted(formatted_records, key=lambda k: k['sortdate'])
+expired_events = sorted(expired_events, key=lambda k: k['sortdate'],
                         reverse=True)
 
-yaml.dump(formatted_records, default_flow_style=False, allow_unicode=True, stream=open('_data/events.yml', 'w', encoding='utf-8'))
+yaml.dump(formatted_records, default_flow_style=False,
+          allow_unicode=True,
+          stream=open('_data/events.yml',
+                      'w', encoding='utf-8'))
 yaml.dump(expired_events, default_flow_style=False, allow_unicode=True,
           stream=open('_data/past_events.yml', 'w', encoding='utf-8'))
-
